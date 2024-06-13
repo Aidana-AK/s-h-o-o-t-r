6cfb6e14598545f017350c7f759cd9372ac8919d
Aidana-AK <prosto.aida521@gmail.com>
2024-06-13 22:49:58 +0500

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream> 
+using namespace std;
+
+int main()
+{
+    string str, word, s = "";
+    cout << "Enter text: ";
+    getline(cin, str);
+
+    stringstream words(str);
+    while (words >> word)
+    {
+        s = word + " " + s;
+    }
+    s.pop_back();
+    cout << "Result: ";
+    cout << s << endl;
+
+    system("pause");
+    return 0;
+}
